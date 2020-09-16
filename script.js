ba77c6659ad3f1d3a815c78fea3f706b136ccd7a
Logan Morro <loganfmorro@gmail.com>
2020-09-16 16:57:16 -0400

    Got the randomization and character count working

--- script.js
+++ script.js
@@ -12,17 +12,42 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+    
 function generatePassword() {
-  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
-  var numeric = '0123456789'
-  var char = '!@#$%^&*()_~[]?;:{}'
-  let password = '';
 
+    let password = '';
+    const numbers = confirm('Do you want numbers?');
+    const upper = confirm('Do you want upper case letters?');
+    const lower = confirm('Do you want lower case letters?');
+    const special = confirm('Do you want special characters?');
+    const length = prompt("How long do you want the password to be? (Must be 8-128 characters)");
+    
+    var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+    var numeric = '0123456789'
+    var char = '!@#$%^&*()_~[]?;:{}'
 
-  var numbers = confirm("Do you want numbers?");
-
+    if (numbers){
+        for(var i=0; i < length; i++){
+            password += numeric.charAt(Math.floor(Math.random() * numeric.length));
+        }
+    }
+    if (upper){
+        for(var i=0; i < length; i++){
+            password += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
+        }
+    }
+    if (lower) {
+        for(var i=0; i < length; i++){
+            password += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
+        }
+    }
+    if (special){
+        for(var i=0; i < length; i++){
+            password += char.charAt(Math.floor(Math.random() * char.length));
+        }
+    }
 
   return password;
 }
+
