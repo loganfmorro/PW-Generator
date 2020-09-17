05140c4b3f74ed653fcf25bf56e14766234e8be5
Logan Morro <loganfmorro@gmail.com>
2020-09-16 20:58:51 -0400

    getting the charlength alert set

--- script.js
+++ script.js
@@ -15,18 +15,30 @@ generateBtn.addEventListener("click", writePassword);
     
 function generatePassword() {
 
+    //Command Prompts for password criteria
     let password = '';
     const numbers = confirm('Do you want numbers?');
     const upper = confirm('Do you want upper case letters?');
     const lower = confirm('Do you want lower case letters?');
     const special = confirm('Do you want special characters?');
     const charlength = prompt("How long do you want the password to be? (Must be 8-128 characters)");
+        
     
+    //Criteria Strings for Password
     var charPool = ""
     var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
     var numeric = '0123456789'
     var specChar = '!@#$%^&*()_~[]?;:{}'
+
+    if (charlength >= 8 && charlength <= 128){
+        ()
+    }
+
+    else {
+        alert("Number is out of the range. Please choose a number between 8 and 128");
+        prompt("Choose a length of at least 8 characters and no more than 128 characters. Type your number");
+        } 
 
     if (numbers) {
    
